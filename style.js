3036a13ec17e118ee5cdfb8f0b34ca68edc495bc
Ramin jamal <131708198+jamalramin@users.noreply.github.com>
2024-09-08 10:40:50 +0200

    Add files via upload

--- style.js
+++ style.js
@@ -1,32 +1,25 @@
 function sendMail() {
 	var fname = document.getElementById("name").value;
-    var lname = document.getElementById("lname").value;
+	var lname = document.getElementById("lname").value;
 	var email = document.getElementById("email").value;
 	var gmassage = document.getElementById("gmassage").value;
 
-    // Check if any of the required fields are empty
+	// Check if any of the required fields are empty
 	if (fname === "" || fname.length > 10 || fname.length < 3) {
-		alert("Your name is invalid !")
-		return false
+		alert("Your name is invalid !");
+		return false;
+	} else if (lname === "" || lname.length > 10 || lname.length < 3) {
+		alert("Your last name is invalid !");
+		return false;
+	} else if (!email.includes("@")) {
+		alert("Your Email is not correct !");
+		return false;
+	} else if (gmassage === "") {
+		alert("Message part can't be empty !");
+		return false;
 	}
-	else if (lname === "" || lname.length > 10 || lname.length < 3) {
-		alert("Your last name is invalid !")
-		return false
-	}
-	else if (!email.includes('@')) {
-		alert("Your Email is not correct !")
-		return false
-	}
-	else if (gmassage === "") {
-		alert("Message part can't be empty !")
-		return false
-	}
-	
+
 	document.getElementById("submit").value = "Sending...";
-	document.getElementById("name").value = "";
-	document.getElementById("lname").value = "";
-	document.getElementById("email").value = "";
-	document.getElementById("gmassage").value = "";
 	document.querySelector(".popup").style.transitionDuration = "0.5s";
 	document.querySelector(".popup").style.transform = "scale(1)";
 
@@ -34,11 +27,18 @@ function sendMail() {
 		name: document.getElementById("name").value,
 		lname: document.getElementById("lname").value,
 		email: document.getElementById("email").value,
+		gmassage: document.getElementById("gmassage").value,
 	};
+
+	document.getElementById("name").value = "";
+	document.getElementById("lname").value = "";
+	document.getElementById("email").value = "";
+	document.getElementById("gmassage").value = "";
+
+	
 	emailjs
 		.send("service_9caae1v", "template_s87404r", params)
 		.then(function (res) {});
-
 	return true;
 }
 
